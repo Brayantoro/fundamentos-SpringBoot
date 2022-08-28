00a6b715db44e6a09e550cc779d5a404de91dcd0
Brayantoro <92780108+Brayantoro@users.noreply.github.com>
2022-08-28 17:10:45 -0500

    primer commit

---
+++ src/main/java/com/fundamentosplatzi/fundamentos/FundamentosSprinbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSprinbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSprinbootApplication.class, args);
+	}
+
+}
